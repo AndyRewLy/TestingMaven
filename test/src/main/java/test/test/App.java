7d65b61d18a460dec5a1a8444e41f7db1eeb4b46
Andrew <andyrewly@gmail.com>
2017-04-03 14:50:40 -0700

    Added a test print

--- test/src/main/java/test/test/App.java
+++ test/src/main/java/test/test/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Test test = new Test();
+    	test.printTest();
         System.out.println( "Hello World0!" );
     }
 }
